70850bae162b6fc56894163a9b7b317f2d3d2c36
Arkadii Shvartsman <a.shvartcman@gmail.com>
2024-01-25 19:58:26 +0100

    fix connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
+console.log(process.env['DATABASE_NAME'], process.env['DATABASE_URI_NO_AUTH']);
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ThrottlerModule.forRoot(),
-    MongooseModule.forRoot(process.env.DATABASE_URI, {
+    MongooseModule.forRoot(process.env.DATABASE_URI_NO_AUTH, {
       dbName: process.env.DATABASE_NAME,
       auth: {
         username: process.env.DATABASE_USER,
